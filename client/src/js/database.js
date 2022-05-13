e04f38ad2605acb23ab0d125ef1f0e36fa09f6be
Dom <Domalbano35@gmail.com>
2022-05-12 20:14:03 -0400

    fixed put and get db functions on db

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,14 +13,19 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (id, content) => {
+export const putDb = async (content) => {
   console.log("PUT to the database");
   const jateDb = await openDB("jate", 1);
   const tx = jateDb.transaction("jate", "readwrite");
   const store = tx.objectStore("jate");
-  const request = store.put({ id: id, todo: content });
+  const request = store.put({ id: 1, todo: content });
   const result = await request;
   console.log("🚀 - data saved to the database", result);
+  if (result) {
+    console.log("saved to db", result);
+  } else {
+    console.error("did not work");
+  }
 };
 
 // TODO: Add logic for a method that gets all the content from the database
@@ -31,8 +36,12 @@ export const getAllDb = async () => {
   const store = tx.objectStore("jate");
   const request = store.getAll();
   const result = await request;
-  console.log("result.value", result);
-  return result;
+  if (result) {
+    console.log("result.value", result);
+    return result.value;
+  } else {
+    console.error("not implemented");
+  }
 };
 
 initdb();
